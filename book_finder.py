b8c05caadeeb23d5e4cf4c2b710ac561dbfa9d5e
Jaime Bustos <jaimebustos130@gmail.com>
2023-12-27 16:16:40 -0500

    Implement partial process_image function for pytesseract readability Comment lines for implementation Test and work with different ways to improve readability for pytessaract

--- book_finder.py
+++ book_finder.py
@@ -21,16 +21,49 @@ Display Results: Display the results back to the user.
 
 """
 import os
-import cv2
-import pytesseract
+import cv2 as cv # Computer vision library to process and edit our image
+import sys
+import pytesseract # OCR library to extract text from image
 import pandas as pd
+
+
+# Function to process the image and prepare for text extraction
+def process_image(image_path):
+    img = cv.imread(cv.samples.findFile(image_path)) # Import the image using cv2
+
+    if img is None: # Safety stop incase image does not exist
+        sys.exit("Could not read the image.")
+    
+    gray_scale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # Convert the image into grayscale
+    high_contrast_img = cv.equalizeHist(gray_scale_img) # Increase contrast of image
+    
+
+
+
+
+    #d = pytesseract.image_to_data(gray_scale_img, output_type=pytesseract.Output.DICT) # Dectecting text in image to increase contrast in its area
+    #print(d) 
+
+    
+
+    cv.imshow("Display window", high_contrast_img) # Display the image for test purposes
+
+    k = cv.waitKey(0) # A way to exit the window
+
+    if k == ord("s"): # If 's' key is pressed, save the image as the following .png file and close the window
+        cv.imwrite("book_spines.png", high_contrast_img)
+    
+
+
 
 # Function to process image and extract text
 def extract_titles(image_path):
     # Read and preprocess image
     # Extract text using pytesseract
-    # Return list of titles\
-    
+    # Return list of titles
+    process_image(image_path)
+
+
     pass
 
 # Function to search titles in the DataFrame
@@ -46,8 +79,9 @@ def main():
     os.chdir(script_directory)
     print("Current Working Directory:", os.getcwd())
 
+    pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
-    image_path = '/path_to_image.jpg'
+    image_path = '../book_finder/book_spine_test.jpg'
     titles = extract_titles(image_path)
     
     # Create DataFrame and export to Excel
